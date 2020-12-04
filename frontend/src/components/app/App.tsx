314009cbd790dcb3345547e9d830274849edcfb5
kr1skyyy <kr1skyyy@abv.bg>
2020-12-04 19:27:43 +0200

    initial project setup

---
+++ frontend/src/components/app/App.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './App.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache()
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
