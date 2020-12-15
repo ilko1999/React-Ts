5628f31db31f07d581cefad4c8573b8ae06b71ee
Iliyan <ilkoignatov76@gmail.com>
2020-12-15 19:11:55 +0200

    Adding a player component and adding a new navbar with effects

--- frontend/src/components/app/App.tsx
+++ frontend/src/components/app/App.tsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './App.css';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import CreateDrawer from './sidebar/drawer';
-import CreateUiComponent from './playerUi/playerComponent'
-import ScrollToTopOnMount from './scrollTop';
+import React from "react";
+import "./App.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import CreateUiComponent from "./playerUi/playerComponent";
+//import Navigation from "./sidebar/navigation";
+import Burger from './sidebar/Burger'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
 });
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <CreateDrawer/>
-      <CreateUiComponent/>
-      <ScrollToTopOnMount />
+      <Burger/>
+        <CreateUiComponent />
     </ApolloProvider>
   );
 }
--- frontend/src/components/app/playerUi/playerComponent.tsx
+++ frontend/src/components/app/playerUi/playerComponent.tsx
@@ -8,6 +8,7 @@ import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
 import SkipPreviousRoundedIcon from '@material-ui/icons/SkipPreviousRounded';
 import SkipNextRoundedIcon from '@material-ui/icons/SkipNextRounded';
 import FavoriteBorderRoundedIcon from '@material-ui/icons/FavoriteBorderRounded';
+import './playerComponent.scss';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -26,23 +27,26 @@ function CreateUiComponent() {
         <React.Fragment>
             <CssBaseline />
             <Container fixed>
-                <Typography component="div" style={{boxShadow: '5px 10px 8px 10px #888888' , backgroundColor: '#FAFAFA' , height: '100px', width: '100%', position: 'fixed', bottom: '0', right: '0', }} >
-                    <div className={classes.root} style = {{display: 'flex', justifyContent: 'center'}}>
-                    <IconButton style = {{display: 'flex'}} color="primary" aria-label="skip to previous song">
-                            <SkipPreviousRoundedIcon fontSize = 'small'/>
+                <Typography id ="containButtons" component="div" >
+                    <div className={classes.root} id= "player">
+                            <IconButton  color="primary" aria-label="skip to previous song">
+                            <SkipPreviousRoundedIcon className = "skips"/>
                         </IconButton>
                         <IconButton color="primary" aria-label="play a song">
-                            <PlayArrowRoundedIcon fontSize = 'large' />
+                            <PlayArrowRoundedIcon className = "play" fontSize = 'large'  />
                         </IconButton>
                         <IconButton color="primary" aria-label="skip to next song">
-                            <SkipNextRoundedIcon fontSize = 'small' />
+                            <SkipNextRoundedIcon className = "skips" />
                         </IconButton>
-                        <Typography variant='h3' gutterBottom style={ {display: 'flex', alignItems: 'center', fontSize: '18px'}}>
+                        <Typography variant='h3' id = "songPlaying" gutterBottom>
                             Song name
-                        </Typography>
-                        <IconButton style = {{display: "flex", justifyContent: 'flex-end'}} color="secondary" aria-label="skip to next song">
-                            <FavoriteBorderRoundedIcon />
+                        </Typography>                        
+                        <div className = "like" >
+                            <IconButton color="secondary" aria-label="like song" >
+                            <FavoriteBorderRoundedIcon  fontSize = "large" />
                         </IconButton>
+                        </div>
+                        
                     </div>
                 </Typography>
             </Container>
